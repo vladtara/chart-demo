93951b209c1eebda9ea6181f5a7330f43325e0cd
Tarasov Vladyslav <vlad@glaps.fun>
2023-05-02 17:14:34 +0300

    test2

--- script.sh
+++ script.sh
@@ -2,3 +2,4 @@ flux push artifact oci://ghcr.io/vlidtara/chart-demo/app:1.0.0 \
         --path="./" \
         --source=https://github.com/vlidtara/chart-demo.git \
         --revision="$(git branch --show-current)@sha1:$(git rev-parse HEAD)"
+helm package . 
